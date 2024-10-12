171379af00e7708ddcc0df465a024c8f738e7bf7
Selby Kendrick <selby.kendrick@gmail.com>
2024-10-12 19:04:53 -0400

    add function to ask user for y/n input and returns a bool

--- main.cpp
+++ main.cpp
@@ -17,10 +17,16 @@ void ask_for_scores(double scores[], const int scores_size, string name) {
     }
 }
 
+bool ask_again() {
+    char yes = 'y';
+    cout << "Again? (y/n) ";
+    cin >> yes;
+    return char == 'y' || char == 'Y';
+}
+
 int main() {
     // A change
     // For Quizes
-    char enter_another = 'y';
     bool again = true;
     // do {
     while (again) {
@@ -66,10 +72,7 @@ int main() {
 
         cout << "Final score: " << final_score << endl;
 
-        cout << "Again? (y/n) ";
-        cin >> enter_another;
-        cout << endl;
-        cout << again << endl;
+        again = ask_again();
     }
     // } while (again);
     return 0;
